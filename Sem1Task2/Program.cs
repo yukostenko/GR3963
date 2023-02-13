6d7d99cbd5fa2fa0f73fcbadf875678cc4d4342a
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-13 23:25:57 +0300

    add solution for task2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдает, какое число большее, в какое меньшее.
+
+// ввод первого числа
+Console.Write("Введите первое число: ");
+string? numLineA = Console.ReadLine();
+// ввод второго числа
+Console.Write("Введите второе число: ");
+string? numLineB = Console.ReadLine();
+
+if (numLineA != null && numLineB != null)
+
+{
+    int numA = int.Parse(numLineA);
+    int numB = int.Parse(numLineB);
+
+    int max = numA;
+
+    if (numA > max) max = numA;
+    if (numB > max) max = numB;
+    {
+    Console.Write("max = "+max);
+    }
+}
