c0369cafbe110fa5d8715e1828030cca93c41aaa
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-14 00:22:32 +0300

    add solution for task 2,4

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,24 +1,29 @@
 ﻿// Напишите программу, которая на вход принимает два числа
 // и выдает, какое число большее, в какое меньшее.
 
-// ввод первого числа
+// Ввод первого числа.
 Console.Write("Введите первое число: ");
 string? numLineA = Console.ReadLine();
-// ввод второго числа
+// Ввод второго числа.
 Console.Write("Введите второе число: ");
 string? numLineB = Console.ReadLine();
-
+// Проверяем, чтобы данные не были пустыми.
 if (numLineA != null && numLineB != null)
 
 {
+    // Парсим введеные числа.
     int numA = int.Parse(numLineA);
     int numB = int.Parse(numLineB);
 
     int max = numA;
+    int min = numB;
 
-    if (numA > max) max = numA;
-    if (numB > max) max = numB;
+    if (numA > numB) max = numA; min = numB;
+    if (numB > numA) max = numB; min = numA;
     {
-    Console.Write("max = "+max);
+        Console.Write("max = " + numA);
+        Console.WriteLine("min = " + numB);
     }
 }
+
+
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая на вход принимает три числа 
+// и выдает максимальное из этих чисел.
+
+// Ввод первого числа.
+Console.Write("Введите первое число: ");
+string? numLineA = Console.ReadLine();
+// Ввод второго числа.
+Console.Write("Введите второе число: ");
+string? numLineB = Console.ReadLine();
+// Ввод третьего числа
+Console.Write("Введите третье число: ");
+string? numLineC = Console.ReadLine();
+// Проверяем, чтобы данные были не пустими.
+if (numLineA != null && numLineB != null && numLineC != null)
+
+{
+    // Парсим введеные числа.
+    int numA = int.Parse(numLineA);
+    int numB = int.Parse(numLineB);
+    int numC = int.Parse(numLineC);
+    // Назначаем максимальное число
+    int max = numA;
+
+    if (numA > max) max = numA;
+    if (numB > max) max = numB;
+    if (numC > max) max = numC;
+    {
+    Console.Write("max = "+max);
+    }
+}
