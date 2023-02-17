446c829b15b5cdaba6c2973d74de197b5f53685f
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-17 19:37:53 +0300

    correct solution

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+
+// Вводим число
+Console.WriteLine("Введите число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+
+{
+    int inputNum = int.Parse(inputLine);
+    int evenNum = inputNum % 2;
+    while (inputNum % 2)
+    {
+    evenNum != inputNum % 2;
+    evenNum = inputNum+1;
+    }
+   Console.WriteLine(evenNum +"Это число четное");
+}
+   
