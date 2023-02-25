7f0cb4b20f55c662ab160a823789d0321f69d1e6
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-25 17:40:12 +0300

    add solution for task10

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int secondDigit = (num/10)%10;
+{
+Console.WriteLine("Вторая цифра числа: " + (secondDigit));
+}
+
+
