47ffab199ad78f76e5e6131ceacdb15731ef9784
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-25 12:56:45 +0300

    add solution for task11

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 11
+// Напишите программу, которая выводит случайное
+// трехзначное число и удаляет второую цифру этого числа
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int lastDigit = num % 10;
+int firstDigit = num / 100;
+num = firstDigit*10+lastDigit;
+Console.WriteLine(num);
+
+//Console.WriteLine((num / 100)*10+(num % 10));
