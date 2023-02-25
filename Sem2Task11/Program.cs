cc5bb49ac524b616e70e7ba6eda2911a068e11ef
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-25 13:02:58 +0300

    add solution for task11

--- Sem2Task11/Program.cs
+++ Sem2Task11/Program.cs
@@ -3,10 +3,10 @@
 // трехзначное число и удаляет второую цифру этого числа
 
 int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
+Console.WriteLine("Случайное число - " + num);
 int lastDigit = num % 10;
 int firstDigit = num / 100;
 num = firstDigit*10+lastDigit;
-Console.WriteLine(num);
+Console.WriteLine("Результат работы - " + num);
 
 //Console.WriteLine((num / 100)*10+(num % 10));
