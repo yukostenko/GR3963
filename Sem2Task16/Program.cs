03d23a8e8d3261d1772380bec7e1a591487f83b1
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-25 17:04:46 +0300

    add solution for task11, 12,14

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 16
+// Напишите программу, которая принимает на вход два числа
+// и проверяет является ли одно число квадратом второго.
+
+Console.Write("Введите первое число:");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число:");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+void SqeaTest(int num1, int num2)
+{
+if (num1 = num2*num2)
+{
+    Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не является квадратом числа {num2}");
+}
+}
+
+SqeaTest(numA, numB);
+SqeaTest(numB, numA);
+
